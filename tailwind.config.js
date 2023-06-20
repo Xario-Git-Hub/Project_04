6c63de41388fcdd342a5c796fd54f58d5a2a9c86
Muhammad Umar Ijaz <m.ghaniumarijaz456@gmail.com>
2023-06-20 12:50:58 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./build/public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      screens:{
+        // 'sm':{
+        //   'max':'550px'
+        // }
+      }
+    },
+  },
+  plugins: [],
+}
